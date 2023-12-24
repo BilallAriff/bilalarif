75474c0d0176928b33bc4cef70893514dec7cd59
Bila Arif <bilalarifhussain@gmail.com>
2023-12-24 10:42:34 +0500

    project initialization complete

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col items-center justify-center bg-gray-100 ">
+      <h1 className="text-5xl">Bilal Arif</h1>
+      <h3 className="text-3xl mt-4">Software Engineer on the UX/UI side</h3>
+    </main>
+  );
+}
