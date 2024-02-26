af19f1a4341130bfc3aa2caad0be4d99d657deab
Bila Arif <bilalarifhussain@gmail.com>
2024-02-26 21:14:53 +0500

    version 2 implemented

--- src/app/store/about_me.ts
+++
@@ -1,6 +0,0 @@
-export const aboutMe = {
-  title: "Bilal Arif",
-  subTitle: "Software Engineer who loves to build UX/UI",
-  description:
-    "I've spent 3+ years building apps for startups and large enterprises, focusing mainly on the user-friendly front-end. I also handle the nitty-gritty of the back-end with ease. Constantly picking up new tech skills, I'm here to turn your ideas into functional solutions.",
-};
